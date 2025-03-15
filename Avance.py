3c77334c522b4956b1f2dcbc52fcd57ca2fd755c
Juan <jsfl2006@gmail.com>
2025-03-15 17:49:49 -0500

    Interfaz

---
+++ Avance.py
@@ -0,0 +1,30 @@
+#Programa de agregar contactos-Interfaz
+class ContacApp:
+    def __init__(self):
+        self.contactos = []
+
+    def show_menu(self):
+        while True:
+            print("\n Menu de contactos:")
+            print("1. Agregar contacto.")
+            print("2. Eliminar contacto.")
+            print("3. Buscar contacto.")
+            print("4. Mostrar contactos.")
+            print("5. Salir.")
+            opcion=input("Seleccione una opción: ")
+            if opcion == "1":
+                self.agregar_contacto()
+            elif opcion == "2":
+                self.eliminar_contacto()
+            elif opcion == "3":
+                self.buscar_contacto()
+            elif opcion == "4":
+                self.mostrar_contactos()
+            elif opcion == "5":
+                print("Saliendo del programa...")
+                break
+            else:
+                print("Opción invalida, intente de nuevo")
+
+c=ContacApp()
+c.show_menu()
